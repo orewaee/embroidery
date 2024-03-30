295e275350c3fe3fb56ba37d6b918e4d3cd690a3
orewaee <71517526+orewaee@users.noreply.github.com>
2024-03-31 00:22:57 +0400

    fix: design structure changed

--- models/design.go
+++ models/design.go
@@ -4,7 +4,9 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Design struct {
 	Id        primitive.ObjectID `json:"id" bson:"_id"`
+	Name      string             `json:"name" bson:"name"`
+	Tag       string             `json:"tag" bson:"tag"`
 	Visible   bool               `json:"-" bson:"visible"`
-	Path      string             `json:"path" bson:"path"`
-	Extension string             `json:"extension" bson:"extension"`
+	Path      string             `json:"-" bson:"path"`
+	Extension string             `json:"-" bson:"extension"`
 }
