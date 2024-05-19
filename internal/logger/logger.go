61f15ca15d9885e6cb2910cef5cf077d0ddce8fd
orewaee <orewaee@gmail.com>
2024-05-19 22:12:16 +0400

    fix: minor logger fix
    now the logger creates a directory if it does not exist

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,6 +1,7 @@
 package logger
 
 import (
+	"errors"
 	"fmt"
 	"github.com/rs/zerolog"
 	"os"
@@ -13,6 +14,10 @@ var logger zerolog.Logger
 
 func Get() zerolog.Logger {
 	once.Do(func() {
+		if err := os.Mkdir("logs", os.ModePerm); err != nil && !errors.Is(err, os.ErrExist) {
+			panic("cannot create log directory")
+		}
+
 		file, err := os.OpenFile(
 			fmt.Sprintf("logs/%s.log", time.Now().Format("2006-01-02-15-04-05")),
 			os.O_APPEND|os.O_CREATE|os.O_WRONLY,
@@ -24,7 +29,7 @@ func Get() zerolog.Logger {
 		}
 
 		writer := zerolog.MultiLevelWriter(
-			zerolog.ConsoleWriter{Out: os.Stdout},
+			zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339},
 			file,
 		)
 
